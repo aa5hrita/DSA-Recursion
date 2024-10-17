7bcf3231e352fd4850d0309c81ded613a58c1f7a
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-10-17 20:14:30 +0530

    N to 1

--- src/main/java/org/javadsa/demos/gfgpractice/easy/PrintNto1UsingRecursion.java
+++ src/main/java/org/javadsa/demos/gfgpractice/easy/PrintNto1UsingRecursion.java
@@ -1,18 +1,19 @@
 package org.javadsa.demos.gfgpractice.easy;
 
-public class Print1toNUsingRecursion {
+public class PrintNto1UsingRecursion {
     static void printNumbers(int N) {
-        if (N > 0) {
-            printNumbers(N - 1);
+        if (N > 0)//The loop runs till N>1
+        {
             System.out.print(N + " ");
+            printNumbers(N - 1);
         }
     }
 
     static void printNumbers2(int N) {
         if (N == 0)
             return;
+        System.out.print(N + " "); //print first and recursively call next smaller number
         printNumbers2(N - 1);
-        System.out.print(N + " ");
     }
 
     public static void main(String[] args) {
